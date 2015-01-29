7720d12418783a6ee5076de335cbedcd982e6485
Benjamin Brown <ben.brown@factmint.com>
2015-01-29 15:52:43 +0000

    New directory structure

---
+++ utilities/geometry.js
@@ -0,0 +1,21 @@
+define(function() {
+	return {
+		circle: {
+			tenth: Math.PI * 1/5,
+			eighth: Math.PI * 1/4,
+			quarter: Math.PI * 1/2,
+			half: Math.PI,
+			whole: 2 * Math.PI,
+			getPointOnCircle: function(x, y, radius, angle) {
+				return {
+					x: x + radius * Math.sin(angle),
+					y: y + -radius * Math.cos(angle)
+				};
+			},
+			getAngle: function(value, dataTotal, totalSize) {
+				totalSize = (typeof totalSize === 'undefined') ? this.whole : totalSize;
+				return (totalSize / dataTotal) * value;
+			}
+		}
+	}
+});
