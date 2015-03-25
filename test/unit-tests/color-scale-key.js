605aee7526a9409b4be7ae8885b829b06cfa269a
Benjamin Brown <ben.brown@factmint.com>
2015-03-25 12:43:10 +0000

    Preliminary unit tests

--- test/unit-tests/color-scale-key.js
+++ test/unit-tests/color-scale-key.js
@@ -1,9 +1,11 @@
 "use strict";
 require.config({
 	paths: {
+		"Color-scale-key": "/components/color-scale-key",
+		"number": "/utilities/number",
 		"QUnit": "/node_modules/qunitjs/qunit/qunit",
-		"svg-js": "/node_modules/svg.js/dist/svg",
-		"color-scale-key": "/components/color-scale-key"
+		"scale": "/utilities/scale",
+		"svg-js": "/node_modules/svg.js/dist/svg"
 	},
 	shim: {
 		"QUnit": {
@@ -29,17 +31,109 @@ function createTestContainer(titleText) {
 	return container;
 }
 
-require(["QUnit", "svg-js", "color-scale-key"], function(QUnit, SVG, Color) {
-	
-	var key = colorScaleKey();
-	
-	QUnit.test("A harmonious colour series should produce the correct classes.", function() {
-	
+require(["Color-scale-key", "number", "QUnit", "scale", "svg-js"], function(colorScaleKey, numberUtilities, QUnit, Scale, SVG) {
 
-		QUnit.equal(
-			Color.harmonious(Color.colorWheelClasses.length + 1)[Color.colorWheelClasses.length],
-			Color.colorWheelClasses[0],
-			"A set of harmonious colours that extends the size of the colour wheel should start from the beginning."
+	QUnit.test("A vertical color scale key component should be rendered correctly.", function() {
+		var paper = SVG(createTestContainer("Vertical colour scale key"));
+
+		var scale = Scale()
+			.withIncrements(4)
+			.project(new Scale().domains.RealNumbers(0, 150))
+			.onto(new Scale().ranges.Linear(0, 200));
+
+		var MAX_TINT_NUMBER = 8;
+
+		var tickMarks = scale.domain.increments.map(function(increment, incrementIndex, increments) {
+			var tickMark = {
+				value: increment
+			};
+
+			var tintStepSize = Math.ceil(MAX_TINT_NUMBER / (scale.domain.increments.length - 2));
+			var tintNumber = parseInt(MAX_TINT_NUMBER - incrementIndex * tintStepSize);
+			if (tintNumber > 0) {
+				tickMark.tintClass = "tint-" + tintNumber;
+			}
+
+			return tickMark;
+		});
+
+		var verticalKey = colorScaleKey(
+			paper,
+			tickMarks,
+			"fm-datum-color-wheel-a",
+			scale,
+			"vertical"
+		);
+		verticalKey.move(
+			0,
+			10
+		);
+
+		verticalKey.markValue(10);
+		
+		QUnit.ok(
+			verticalKey instanceof SVG.G,
+			"A vertical color scale key component should return an SVG group."
+		);
+		QUnit.ok(
+			verticalKey.node.querySelectorAll("rect")[0].classList.contains("tint-8"),
+			"A vertical color scale key component should have segments with the correct tint class"
+		);
+		QUnit.ok(
+			verticalKey.node.querySelectorAll("rect")[1].classList.contains("tint-4"),
+			"A vertical color scale key component should have segments with the correct tint class"
+		);
+	});
+
+	QUnit.test("A horizontal color scale key component should be rendered correctly.", function() {
+		var paper = SVG(createTestContainer("Horizontal colour scale key"));
+
+		var scale = Scale()
+			.withIncrements(4)
+			.project(new Scale().domains.RealNumbers(0, 350))
+			.onto(new Scale().ranges.Linear(0, 250));
+
+		var MAX_TINT_NUMBER = 7;
+
+		var tickMarks = scale.domain.increments.map(function(increment, incrementIndex, increments) {
+			var tickMark = {
+				value: increment
+			};
+
+			var tintStepSize = Math.ceil(MAX_TINT_NUMBER / (scale.domain.increments.length - 2));
+			var tintNumber = parseInt(MAX_TINT_NUMBER - incrementIndex * tintStepSize);
+			if (tintNumber > 0) {
+				tickMark.tintClass = "tint-" + tintNumber;
+			}
+
+			return tickMark;
+		});
+
+		var horizontalKey = colorScaleKey(
+			paper,
+			tickMarks,
+			"fm-datum-color-wheel-c",
+			scale,
+			"horizontal"
+		);
+		horizontalKey.move(
+			30,
+			0
+		);
+
+		horizontalKey.markValue(230);
+
+		QUnit.ok(
+			horizontalKey instanceof SVG.G,
+			"A horizontal color scale key component should return an SVG group."
+		);
+		QUnit.ok(
+			horizontalKey.node.querySelectorAll("rect")[0].classList.contains("tint-7"),
+			"A horizontal color scale key component should have segments with the correct tint class"
+		);
+		QUnit.ok(
+			horizontalKey.node.querySelectorAll("rect")[1].classList.contains("tint-3"),
+			"A horizontal color scale key component should have segments with the correct tint class"
 		);
 	});
 
--- test/unit-tests/key.js
+++ test/unit-tests/key.js
@@ -2,11 +2,11 @@
 require.config({
 	paths: {
 		"Color": "/utilities/color",
-		"path": "/node_modules/paths-js",
-		"key": "/components/key",
+		"path": "../../node_modules/paths-js/dist/amd/path",
+		"Key": "/components/key",
 		"QUnit": "/node_modules/qunitjs/qunit/qunit",
 		"svg-js": "/node_modules/svg.js/dist/svg",
-		'Flow': '/inventions/flow',
+		'flow': '/inventions/flow',
 		'float': '/etc/float',
 		'grid': '/etc/grid',
 		'centre': '/etc/centre'
@@ -22,31 +22,46 @@ require.config({
 	}
 });
 
-require(["Color",  "path", "key", "QUnit", "svg-js"], function(Color, path, key, QUnit, SVG) {
+function createTestContainer(titleText) {
+	var container = document.createElement('div');
+	container.className = 'test-svg-container';
+	
+	var title = document.createElement('h4');
+	title.innerHTML = titleText;
+	container.appendChild(title);
+	
+	document.body.appendChild(container);
+	
+	return container;
+}
 
-	function createTestContainer(titleText) {
-		var container = document.createElement('div');
-		container.className = 'test-svg-container';
-		
-		var title = document.createElement('h4');
-		title.innerHTML = titleText;
-		container.appendChild(title);
-		
-		document.body.appendChild(container);
-		
-		return container;
-	}
+require(["Color",  "path", "Key", "QUnit", "svg-js", "flow"], function(Color, path, key, QUnit, SVG, Flow) {
 
-	var chart = createTestContainer("Test container");
+	var paper = SVG(createTestContainer("Test container"));
 
-	QUnit.test("The key component should return an SVG group.", function() {
+	QUnit.test("The key component should render correctly.", function() {
 
 		var width = 200;
 		var values = ["One", "Two", "Three", "Four", "Five", "Six", "Seven"];
 		var colorClasses = Color.harmonious(7);
 
-		var testKey = key(chart, width, values, colorClasses);
+		var testKey = key(paper, width, values, colorClasses);
 
+		QUnit.ok(testKey.node.querySelector(".fm-key-top-border"), "The key should have a top border.");
+
+		QUnit.ok(testKey instanceof SVG.G, "The key component should return an SVG group.");
+
+		QUnit.equal(
+			7,
+			testKey.node.querySelectorAll(".fm-key-value-indicator").length,
+			"There should be seven value indicators when seven values are passed to the key."
+		);
+
+		QUnit.equal(
+			7,
+			testKey.node.querySelectorAll(".fm-key-value-title").length,
+			"There should be seven value titles when seven values are passed to the key."
+		);
 	});
 
 	QUnit.load();
