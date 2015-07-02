aec1b8cc6cd0bed02486e7e2142a177d659e9c3a
Chris Scott <chris.scott@factmint.com>
2015-07-02 12:55:37 +0000

    Added all modules to the global export

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,8 +32,32 @@ module.exports = function(grunt) {
 				        "classList": "node_modules/classList/classList",
 				        "path": "node_modules/paths-js/dist/amd/path",
 				        "svg-js": "node_modules/svg.js/dist/svg",
+				        
 				        "circle-segment": "inventions/circle-segment",
-				        "geometry": "utilities/geometry"
+				        "doughnut-segment": "inventions/doughnut-segment",
+				        "dashed-bracket": "inventions/dashed-bracket",
+				        "flow": "inventions/flow",
+				        "tooltip-background": "inventions/tooltip-background",
+				        
+				        "G.unshift": "extensions/G.unshift",
+				        
+				        "centre": "etc/centre",
+				        "float": "etc/float",
+				        "grid": "etc/grid",
+				        
+				        "key": "components/key",
+				        "color-scale-key": "components/color-scale-key",
+				        "tooltip": "components/tooltip",
+				        "two-section-tooltip": "components/two-section-tooltip",
+				        "multi-measure-tooltip": "components/multi-measure-tooltip",
+				        "text-area": "components/text-area",
+				        
+				        "geometry": "utilities/geometry",
+				        "number": "utilities/number",
+				        "scale": "utilities/scale",
+				        "state": "utilities/state",
+				        "mapper": "utilities/mapper",
+				        "color": "utilities/color"
 					},
 					shim: {
 						'svg-js': {
--- factmint-charts.js
+++ factmint-charts.js
@@ -6,11 +6,59 @@ window['factmint'] = {
 
 require([
    "svg-js",
-   "circle-segment"
+   "color",
+   "geometry",
+   "mapper",
+   "number",
+   "scale",
+   "state",
+   "key",
+   "color-scale-key",
+   "tooltip",
+   "two-section-tooltip",
+   "multi-measure-tooltip",
+   "text-area",
+   "G.unshift",
+   "circle-segment",
+   "doughnut-segment",
+   "flow",
+   "dashed-bracket"
 ], function(
-    SVG
+    SVG,
+    colorUtils,
+    geometryUtils,
+    mapperUtils,
+    numberUtils,
+    scaleUtils,
+    stateUtils,
+    keyComponent,
+    colorScaleKeyComponent,
+    tooltipComponent,
+    twoSectionTooltipComponent,
+    multiMeasureTooltipComponent,
+    textAreaComponent
 ) {
-    window.factmint.SVG = SVG;
+    window.factmint.draw = function(parent) {
+        return new SVG(parent);
+    }
+    
+    window.factmint.utilities = {
+        color: colorUtils,
+        geometry: geometryUtils,
+        mapper: mapperUtils,
+        number: numberUtils,
+        scale: scaleUtils,
+        state: stateUtils
+    };
+    
+    window.factmint.components = {
+        key: keyComponent,
+        colorScaleKey: colorScaleKeyComponent,
+        tooltip: tooltipComponent,
+        twoSectionTooltip: twoSectionTooltipComponent,
+        multiMeasureTooltip: multiMeasureTooltipComponent,
+        textArea: textAreaComponent
+    }
     
     window.factmint._onready();
 });
