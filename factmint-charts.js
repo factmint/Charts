755c3c44d8c0349156fe3947577bda23d91b90cf
Chris Scott <chris.scott@factmint.com>
2015-07-02 12:37:58 +0000

    Removed config as it's supplied in the Gruntfile

--- factmint-charts.js
+++ factmint-charts.js
@@ -4,16 +4,6 @@ window['factmint'] = {
   }
 };
 
-require.config({
-    paths: {
-        "classList": "node_modules/classList/classList",
-        "path": "node_modules/paths-js/dist/amd/path",
-        "svg-js": "node_modules/svg.js/dist/svg",
-        "circle-segment": "inventions/circle-segment",
-        "geometry": "utilities/geometry"
-    }
-});
-
 require([
    "svg-js",
    "circle-segment"
