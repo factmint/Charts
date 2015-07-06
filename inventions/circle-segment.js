4e799cf4d4ee9bd1eafa5fe07f29136656c663d1
Ben Brown <benjaminbrown92@googlemail.com>
2015-07-06 14:01:28 +0000

    Rename x and y to xOrigin and yOrigin to clarify that they are not the top left corners as would be for other shapes

--- inventions/circle-segment.js
+++ inventions/circle-segment.js
@@ -14,14 +14,14 @@ define([
 		inherit: SVG.Path,
 
 		construct: {
-			circleSegment: function(x, y, radius, startAngle, endAngle) {
-				var point1 = Geometry.circle.getPointOnCircumference(x, y, radius, startAngle);
-				var point2 = Geometry.circle.getPointOnCircumference(x, y, radius, endAngle);
+			circleSegment: function(xOrigin, yOrigin, radius, startAngle, endAngle) {
+				var point1 = Geometry.circle.getPointOnCircumference(xOrigin, yOrigin, radius, startAngle);
+				var point2 = Geometry.circle.getPointOnCircumference(xOrigin, yOrigin, radius, endAngle);
 
 				var large = ((endAngle - startAngle) > Math.PI) ? 1 : 0;
 
 				var segmentPathString = Path()
-					.moveto(x, y)
+					.moveto(xOrigin, yOrigin)
 					.lineto(point1.x, point1.y)
 					.arc(radius, radius, 0, large, 1, point2.x, point2.y)
 					.closepath()
