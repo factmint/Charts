5777a1fc64771a3ca93b4b02696c9bd652fd29e6
Ben Brown <benjaminbrown92@googlemail.com>
2015-10-12 07:49:43 +0000

    Rename directories

---
+++ charts/choropleth-uk-constituencies/Gruntfile.js
@@ -0,0 +1,125 @@
+module.exports = function(grunt) {
+	// Load all tasks
+	require('load-grunt-tasks')(grunt);
+	// Show elapsed time
+	require('time-grunt')(grunt);
+	
+	var dependencies = grunt.file.readJSON('dependencies.json');
+	
+	grunt.initConfig ({
+		pkg: grunt.file.readJSON('package.json'),
+		connect: {
+			options: {
+				base: '../../'
+			},
+			dev: {
+				options: {
+					port: 15009
+				}
+			},
+			test: {
+				options: {
+					keepalive: false,
+					port: 15008
+				}
+			}
+		},
+		qunit: {
+			tests: {
+				options: {
+					httpBase: 'http://localhost:15008'
+				},
+				src: 'test/*.html'
+			}
+		},
+		clean: {
+			release: {
+				src: ['dist/'],
+			},
+			up: {
+				src: ['temp']
+			}
+		},
+		sass: {
+			dev: {
+				files: {
+					'src/style/chart.css' : 'src/style/chart.scss'
+				},
+			},
+			release: {
+				files: {
+					'temp/<%= pkg.releaseName %>.min.css' : 'src/style/chart.scss'
+				},
+				options: {
+					style: 'compressed',
+				}
+			}
+		},
+		autoprefixer: {
+			release: {
+				expand: true,
+				flatten: true,
+				src: 'temp/<%= pkg.releaseName %>.min.css',
+				dest: 'dist/'
+			}
+		},
+		requirejs: {
+			release: {
+				options: {
+					baseUrl: "./src/scripts",
+					name: "almond",
+					include: ["main"],
+					out: "dist/<%= pkg.releaseName %>.js",
+					paths: dependencies,
+					shim: {
+						'svg-js': {
+							exports: 'SVG'
+						}
+					},
+					optimize: 'none'
+				}
+			}
+		},
+		closurecompiler: {
+			release: {
+				files: {
+					'dist/<%= pkg.releaseName %>.min.js': ['dist/<%= pkg.releaseName %>.js']
+				},
+				options: {
+					'language_in': 'ECMASCRIPT5',
+					'compilation_level': 'SIMPLE_OPTIMIZATIONS',
+					'banner': '/* <%= pkg.name %> version <%= pkg.version %>. Copyright (c) Factmint Ltd (http://factmint.com); Licensed under the MIT License (http://opensource.org/licenses/MIT); includes SVG.js, https://github.com/wout/svg.js/ */'
+				}
+			}
+		},
+		exec: {
+			document: 'grep --only-matching --no-filename "options\\.[a-zA-Z]*" src/scripts/*.js | sort | uniq | cut -d. -f2 > dist/options.txt'
+		},
+		watch: {
+            options: {
+                livereload: true
+            },
+			css: {
+				files: ['src/style/*.scss'],
+				tasks: ['sass']
+			},
+			js: {
+				files: ['src/scripts/*.js'],
+			}
+		}
+	});
+
+
+	grunt.registerTask('serve', ['sass:dev', 'connect:dev', 'openport:watch.options.livereload:35729:40000', 'watch']);
+	grunt.registerTask('test', ['sass', 'autoprefixer', 'closurecompiler', 'connect:test', 'qunit']);
+	grunt.registerTask('build', [
+		'clean:release',			// Make sure no files from previous releases are left around
+		'requirejs',				// Build the r.js single file script
+		'copy',						// Remove //NODRM and //COMMERCIALUSE comments
+		'closurecompiler',			// Minify
+		'sass:release',				// Generate CSS
+		'autoprefixer:release',		// Prefix CSS
+		'clean:up',					// Clean up the temp directory(s)
+		'exec:document',			// Document the use of any options (looks for pattern /options.[a-zA-Z]*/)
+	]);
+};
